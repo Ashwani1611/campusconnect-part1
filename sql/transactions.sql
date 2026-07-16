fcae231680fe9fac2f375f2ed2ce73b7b8f2463d
Ashwani1611 <a4ashwanik4kr@gmail.com>
2026-07-16 12:21:11 +0530

    Step 6: transactions and isolation

--- sql/transactions.sql
+++ sql/transactions.sql
@@ -1,1 +1,17 @@
-HI
+-- Run: sqlite3 campusconnect.db < sql/transactions.sql
+PRAGMA foreign_keys = ON;
+
+BEGIN TRANSACTION;
+
+UPDATE courses
+SET seats_taken = seats_taken + 1
+WHERE course_id = 9
+  AND seats_taken < seats_total;
+
+INSERT INTO enrollments (enrollment_id, student_id, course_id, enrolled_at, status)
+VALUES (13, 11, 9, '2025-08-11 09:00:00', 'active');
+
+COMMIT;
+
+SELECT course_id, seats_taken, seats_total FROM courses WHERE course_id = 9;
+SELECT * FROM enrollments WHERE enrollment_id = 13;
